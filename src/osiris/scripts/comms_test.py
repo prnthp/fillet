754646d7d5aa73d61eac8c0c5379eafbd5ef23c9
mr-wsinw <prnthp@uw.edu>
2017-06-17 20:32:34 -0500

    Add communication test service and tidied up ganesh

---
+++ src/osiris/scripts/comms_test.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# Comms Test: Recieves string and spits out md5 hash of string
+import rospy
+import md5
+from osiris.srv import *
+
+def handle_comms_test(req):
+    return comms_test_srvResponse(md5.new(req.input).hexdigest())
+
+def comms_test_server():
+    rospy.init_node('comms_test_server', anonymous=True)
+    s = rospy.Service('comms_test', comms_test_srv, handle_comms_test)
+    rospy.loginfo("Comms Test: Service started")
+
+    rospy.spin()
+
+if __name__ == '__main__':
+    comms_test_server()
--- src/osiris/scripts/ganesh.py
+++ src/osiris/scripts/ganesh.py
@@ -7,32 +7,43 @@ from std_msgs.msg import String
 from osiris.srv import *
 
 def handle_ganesh(req):
+
+  global name
+
   if req.command == "recordbegin":
     rospy.loginfo("Ganesh: Starting rosbag")
+    # TODO: Add try-catch
     # TODO: Add information to bag filename
+
     name = req.filename
+
     topics = ""
     for topic in req.topics:
         topics += topic + " "
+
     command = "rosbag record -o " + name + " " + topics
     dir_save_bagfile = os.path.expanduser("~") + "/Record/"
     rosbag_proc = subprocess.Popen(command, stdin=subprocess.PIPE, shell=True, cwd=dir_save_bagfile)
     return ganesh_srvResponse(1,name)
+
   elif req.command == "recordend":
-    # http://answers.ros.org/question/10714/start-and-stop-rosbag-within-a-python-script/
-    # Holy mother of hacks
+    # TODO: Add try-catch
+    # HACK: http://answers.ros.org/question/10714/start-and-stop-rosbag-within-a-python-script/
     list_cmd = subprocess.Popen("rosnode list", shell=True, stdout=subprocess.PIPE)
     list_output = list_cmd.stdout.read()
     retcode = list_cmd.wait()
     assert retcode == 0, "List command returned %d" % retcode
+
     for str in list_output.split("\n"):
         if (str.startswith("/record")):
             os.system("rosnode kill " + str)
             rospy.loginfo("Ganesh: Killed rosbag node: " + str)
+
     rospy.logwarn("Ganesh: Recording ended")
-    name = ""
-    # TODO: FIX RETURN NAME
     return ganesh_srvResponse(1,name)
+
+  else:
+    return ganesh_srvResponse(0,"Uknown Command")
 
 def ganesh_server():
   rospy.init_node('ganesh_server', anonymous=True)
