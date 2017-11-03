3405c4380125b796dddd908dfdc30e5636afdf45
mr-wsinw <prnthp@uw.edu>
2017-11-03 16:36:06 -0700

    Test talker for unity subscribe

--- src/osiris/scripts/talker.py
+++ src/osiris/scripts/talker.py
@@ -1,11 +1,19 @@
-#!/usr/bin/evn python
+#!/usr/bin/env python
 import rospy
 from osiris.msg import *
 
 def talker():
     pub = rospy.Publisher('testlisten', GameObject, queue_size=10)
     rospy.init_node('talker', anonymous=True)
-    rate = rospy.rate(200) # 200 Hz, faster than Unity
+    rate = rospy.Rate(200) # 200 Hz, faster than Unity
     while not rospy.is_shutdown():
         go = GameObject()
-        
+        go.time = 1.0
+        pub.publish(go)
+        rate.sleep()
+
+if __name__ == '__main__':
+    try:
+        talker()
+    except rospy.ROSInterruptException:
+        pass
