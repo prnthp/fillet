0bc470011c78e460602f44b1191a978f43f43651
mr-wsinw <prnthp@uw.edu>
2017-11-06 19:36:04 -0800

    Talker now publishes full messages

--- src/osiris/scripts/talker.py
+++ src/osiris/scripts/talker.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import rospy
 from osiris.msg import *
+from geometry_msgs import Pose
 
 def talker():
     pub = rospy.Publisher('testlisten', GameObject, queue_size=10)
@@ -8,7 +9,28 @@ def talker():
     rate = rospy.Rate(200) # 200 Hz, faster than Unity
     while not rospy.is_shutdown():
         go = GameObject()
-        go.time = 1.0
+        go.unique_id = 1
+        go.frame_count = 2
+        go.time = 3.5
+        go.parent = '4.0'
+        go.num_poses = 5
+        go.poses = []
+        temp = Pose()
+        temp.position.x = 1
+        temp.position.y = 2
+        temp.position.z = 3
+        go.poses.append(temp)
+        temp.position.x = 3
+        temp.position.y = 2
+        temp.position.z = 1
+        go.poses.append(temp)
+        go.has_event = True
+        go.events = ['stop','drop','rooooooool']
+        go.has_values = True
+        go.values = []
+        go.values.append(Values('weight',10.1))
+        go.values.append(Values('height',99.9999990))
+        
         pub.publish(go)
         rate.sleep()
 
