a40293436f5aa1aa2b39631f88c991894a2fc7f3
prnthp <prnthp@uw.edu>
2017-11-10 10:24:35 -0800

    Republisher node for arduino and updated .gitignore

--- src/osiris/scripts/republish.py
+++ src/osiris/scripts/republish.py
@@ -4,17 +4,23 @@ from osiris.msg import *
 from geometry_msgs.msg import Pose
 from std_msgs.msg import *
 
-pub = rospy.Pubisher('replushed_float', GameObject, queue_size=10)
+pub = rospy.Publisher('republished_float', GameObject, queue_size=10)
 
 def callback(input_float):
     go = GameObject()
     go.unique_id = 0
     go.has_values = True
     go.values = []
-    go.values.append(Values('weight',input_float.data))
+    go.values.append(Values('length',input_float.data))
     pub.publish(go)
 
 def republish():
     rospy.init_node('republisher')
-    rospy.Subscriber('simple_float', Float64, callback)
+    rospy.Subscriber('pressure0', Float64, callback)
     rospy.spin()
+
+if __name__ == '__main__':
+    try:
+        republish()
+    except rospy.ROSInterruptException:
+        pass
